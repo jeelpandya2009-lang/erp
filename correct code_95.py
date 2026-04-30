896cae7f1b9bea392518481e7ccfafb6c3964f14
jeelpandya2009-lang <jeel.pandya2009@gmail.com>
2026-04-30 14:11:47 +0530

    Create correct code_95.py

---
+++ correct code_95.py
@@ -0,0 +1,27 @@
+print("Range Counter")
+
+start = int(input("Enter start: "))
+end = int(input("Enter end: "))
+
+even = 0
+odd = 0
+sum_total = 0
+
+while start <= end:
+    sum_total = sum_total + start
+    
+    if start % 2 == 0:
+        even = even + 1
+    else:
+        odd = odd + 1
+    
+    start = start + 1
+
+print("Even count:", even)
+print("Odd count:", odd)
+print("Sum:", sum_total)
+
+if even > odd:
+    print("More even numbers")
+else:
+    print("More odd numbers")
